5420d9182d69f4d1d7fde149fc84eaeb811cbb8e
summitkummar <summitenterprises27@gmail.com>
2026-09-18 15:40:37 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'teachdesk-v3';
+const urlsToCache = ['./', './index.html', './manifest.json'];
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache =>
+      Promise.all(urlsToCache.map(url => cache.add(url).catch(() => {})))
+    )
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(names =>
+      Promise.all(names.filter(n => n !== CACHE_NAME).map(n => caches.delete(n)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  const req = event.request;
+  if (req.method !== 'GET') return;
+  event.respondWith(
+    caches.match(req).then(cached => {
+      const fetchPromise = fetch(req).then(res => {
+        if (res && res.status === 200 && res.type === 'basic') {
+          const clone = res.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put(req, clone));
+        }
+        return res;
+      }).catch(() => cached);
+      return cached || fetchPromise;
+    })
+  );
+});
